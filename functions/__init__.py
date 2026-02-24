dd6a802018548d8a0e1fff10690f90597140eba3
MatthewJames6184 <ariasjamesmatthew@gmail.com>
2026-02-24 14:33:40 +0800

    fix feat error

---
+++ functions/__init__.py
@@ -0,0 +1,1 @@
+# makes "functions" a package
--- functions/scan_input_folder.py
+++ functions/scan_input_folder.py
@@ -1,9 +1,9 @@
 import os
 
 
-def scan_input_folder_for_new_csv():
-	# STUB — replace with real logic when Data Processing Lead is ready
-	input_folder = "input"
+def scan_input_folder_for_new_csv(input_folder="input"):
+	if not os.path.isdir(input_folder):
+		return []
 	csv_files = [f for f in os.listdir(input_folder) if f.endswith(".csv")]
 	print(f"Found: {csv_files}")
 	return csv_files
